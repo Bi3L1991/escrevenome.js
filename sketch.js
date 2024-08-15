42fde64e1d262a4d9d34a6a19ee02b5bb86131be
Bi3L1991 <jair_gabriel15@hotmail.com>
2024-08-15 08:10:43 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,34 @@
+let palavra;
+
+function setup() {
+  createCanvas(400, 400);
+
+  palavra = palavraAleatoria();
+  
+}
+
+function palavraAleatoria() {
+  
+  let palavras = ["Caminhante", "Caminho", "Caminha"];
+  
+  return random(palavras);
+}
+
+function inicializaCores() {
+  background("white");
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+}
+
+function palavraParcial(minimo, maximo) {
+  let quantidade = map(mouseX, minimo, maximo, 1, palavra.length);
+  let parcial = palavra.substring(0, quantidade);
+  return parcial;
+}
+
+function draw() {
+  
+  inicializaCores();
+
+  let parcial = palavraParcial(0, width);
